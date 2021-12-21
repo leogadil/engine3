1bb3a44b6f9753dbb7bea0f402557deb473b6aeb
leogadil <leogadilvlog@gmail.com>
2021-12-21 22:23:27 +0800

    make loader usable

--- framework/mloader.py
+++ framework/mloader.py
@@ -1,19 +1,39 @@
 
 import os
+from importlib.machinery import SourceFileLoader
 from pathlib import Path
+
+from logger import get_logger
+
+lgr = get_logger(__name__)
 
 class loader:
 
     def __init__(self, path: str):
         self.path = os.path.join(Path(__file__).parent, path)
         self.files = []
-        self.load()
+        self.modules = {}
 
-    def load(self) -> None:
+    def load(self) -> object:
+        self.get_modules()
+        
+        if self.files:
+            for path, name in self.files:
+                mm = SourceFileLoader(name, path).load_module()
+                try:
+                    main = getattr(mm, name)
+                    self.modules[name] = main()
+                except AttributeError as e:
+                    lgr.warning(f"Module {name} can't be found. {e}")
+
+        return self
+
+    def get_modules(self) -> None:
+        self.files = []
         for root, dirs, files in os.walk(self.path):
             for file in files:
                 if file.endswith(".py"):
-                    self.files.append(os.path.join(root, file))
+                    self.files.append([os.path.join(root, file), file.split(".")[0]])
 
     def get_files(self) -> list:
         return self.files
@@ -23,4 +43,4 @@ class loader:
 
 if __name__ == '__main__':
     l = loader('modules')
-    print(l.get_files())
+    print(l.load())
