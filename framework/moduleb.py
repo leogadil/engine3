5cb1e48d011ff1db063050b1f2243b4e9729ce87
leogadil <leogadilvlog@gmail.com>
2021-12-21 22:24:32 +0800

    fixed init and added threading

--- framework/moduleb.py
+++ framework/moduleb.py
@@ -1,15 +1,32 @@
 
+import abc
+import threading
+
 import crud
 from logger import get_logger
+
 
 class mobase(crud.database):
 
     def __init__(self, name, desc, ver) -> None:
-        self.name = name | "Module"
-        self.description = desc | "This is a sample Description"
-        self.version = ver | "0.0.1"
+        self.name = name
+        self.description = desc
+        self.version = ver
 
     def logger(self, name):
         return get_logger(name)
+
+    @abc.abstractmethod
+    def run(self):
+        pass
+    
+    @staticmethod
+    def run_in_thread(fn):
+        def run(*k, **kw):
+            t = threading.Thread(target=fn, args=k, kwargs=kw)
+            t.daemon = True
+            t.start()
+            return t # <-- this is new!
+        return run
 
     
