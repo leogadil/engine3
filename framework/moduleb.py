fe091293f59d65dfb424c0d2a8145f4a1a0c6b73
leogadil <leogadilvlog@gmail.com>
2021-12-22 16:32:47 +0800

    updated

--- framework/moduleb.py
+++ framework/moduleb.py
@@ -1,17 +1,30 @@
 
 import abc
+import os
 import threading
+import json
+from idgen import genid
 
 import crud
 from logger import get_logger
 
+lgr = get_logger(__name__)
 
 class mobase(crud.database):
 
     def __init__(self, name, desc, ver) -> None:
-        self.name = name
+        self.name = name.lower()
         self.description = desc
         self.version = ver
+        self.http_host = None
+        folder = 'framework/modules/{}'.format(self.name)
+        if not os.path.exists(folder):
+            lgr.warning('Creating folder: {}'.format(folder))
+            os.makedirs(folder)
+
+        self.config = json.load(open('framework/config.json'))
+
+        super().__init__('modules/{}/database.db'.format(self.name))
 
     def logger(self, name):
         return get_logger(name)
@@ -19,6 +32,9 @@ class mobase(crud.database):
     @abc.abstractmethod
     def run(self):
         pass
+
+    def create_id(self):
+        return "{}_{}".format(self.name, genid())
     
     @staticmethod
     def run_in_thread(fn):
